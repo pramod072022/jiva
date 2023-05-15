07663c1c5de0f0353da378523a8503ef4ff791ac
pramod072022 <pramodt072021@gmail.com>
2023-05-15 12:44:37 +0000

    compute

---
+++ compute/variable.tf
@@ -0,0 +1,12 @@
+# ----------- Compute variable ---
+
+variable "bastion-server-instance-type" {}
+
+variable "cluster-name" {}
+
+variable "key_name" {}
+
+variable "aws_subnet" {}
+
+variable "vpc_id" {}
+
